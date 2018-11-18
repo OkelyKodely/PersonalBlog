e14735f1104b4e79334271a31d60f2652c732b2b
Daniel Cho <dh.cho428@gmail.com>
2018-11-18 00:07:40 -0800

    Add files via upload

--- PersonalBlog.java
+++ PersonalBlog.java
@@ -272,26 +272,129 @@ public class PersonalBlog extends javax.swing.JFrame {
     
     public class MyCellRenderer extends JPanel implements ListCellRenderer {
 
-        private JLabel image, left, right;
+        private JLabel image1, image2, image3, image4, left, right;
 
         private MyCellRenderer() {
-            setAlignmentX(0);
-            
-            image    = new JLabel();
+            image1    = new JLabel();
+            image2    = new JLabel();
+            image3    = new JLabel();
+            image4    = new JLabel();
             left      = new JLabel();
             right      = new JLabel();
-            add(image);
+            add(image1);
+            add(image2);
+            add(image3);
+            add(image4);
             add(left);
             add(right);
         }
 
         public Component getListCellRendererComponent(JList list,Object value,int index,boolean isSelected,boolean cellHasFocus){
             try {
-                Image i = ImageIO.read(new File("missile.png"));
+                System.out.println((index + "").length());
+                String v = index + "";
+                String v1 = "", v2 = "", v3 = "", v4 = "";
+                if(index > -1) {
+                    v1 = v.substring(0, 1);
+                }
+                else {
+//                    remove(image1);
+                }
+                Image i = ImageIO.read(new File(v1 + ".jpg"));
                 ImageIcon imageMap = new ImageIcon(i);
-                image.setIcon(imageMap);
-                image.setHorizontalTextPosition(JLabel.RIGHT);
-                image.setText("");
+                image1.setIcon(imageMap);
+                image1.setHorizontalTextPosition(JLabel.RIGHT);
+                image1.setText("");
+                if(index > 9) {
+                    v2 = v.substring(1, 2);
+                    i = ImageIO.read(new File(v2 + ".jpg"));
+                    imageMap = new ImageIcon(i);
+                    boolean wasNull = false;
+                    if(image2 == null) {
+                        image2    = new JLabel();
+                        wasNull = true;
+                    }
+                    image2.setIcon(imageMap);
+                    image2.setHorizontalTextPosition(JLabel.RIGHT);
+                    image2.setText("");
+                    if(wasNull)
+                        add(image2);
+//                    remove(image2);
+//                    add(image2);
+                }
+                else {
+                    i = ImageIO.read(new File("cuang.jpg"));
+                    imageMap = new ImageIcon(i);
+                    boolean wasNull = false;
+                    if(image2 == null) {
+                        image2    = new JLabel();
+                        wasNull = true;
+                    }
+                    image2.setIcon(imageMap);
+                    image2.setHorizontalTextPosition(JLabel.RIGHT);
+                    image2.setText("");
+                    if(wasNull)
+                        add(image2);
+//                    remove(image2);
+//                    add(image2);
+                    //image2 = null;
+                }
+                if(index > 99) {
+                    v3 = v.substring(2, 3);
+                    i = ImageIO.read(new File(v3 + ".jpg"));
+                    imageMap = new ImageIcon(i);
+                    image3.setIcon(imageMap);
+                    image3.setHorizontalTextPosition(JLabel.RIGHT);
+                    image3.setText("");
+//                    remove(image3);
+//                    add(image3);
+                }
+                else {
+//                    remove(image3);
+                    i = ImageIO.read(new File("cuang.jpg"));
+                    imageMap = new ImageIcon(i);
+                    boolean wasNull = false;
+                    if(image3 == null) {
+                        image3    = new JLabel();
+                        wasNull = true;
+                    }
+                    image3.setIcon(imageMap);
+                    image3.setHorizontalTextPosition(JLabel.RIGHT);
+                    image3.setText("");
+                    if(wasNull)
+                        add(image3);
+//                    remove(image3);
+//                    add(image3);
+                    //image3 = null;
+                }
+                if(index > 999) {
+                    v4 = v.substring(3, 4);
+                    i = ImageIO.read(new File(v4 + ".jpg"));
+                    imageMap = new ImageIcon(i);
+                    image4.setIcon(imageMap);
+                    image4.setHorizontalTextPosition(JLabel.RIGHT);
+                    image4.setText("");
+//                    remove(image4);
+//                    add(image4);
+                }
+                else {
+//                    remove(image4);
+                    i = ImageIO.read(new File("cuang.jpg"));
+                    imageMap = new ImageIcon(i);
+                    boolean wasNull = false;
+                    if(image4 == null) {
+                        image4    = new JLabel();
+                        wasNull = true;
+                    }
+                    image4.setIcon(imageMap);
+                    image4.setHorizontalTextPosition(JLabel.RIGHT);
+                    image4.setText("");
+                    if(wasNull)
+                        add(image4);
+//                    remove(image4);
+//                    add(image4);
+                    //image4 = null;
+                }
                 String leftData      = ((String[])value)[0];
                 String rightData      = ((String[])value)[1];
                 left.setText(leftData);
